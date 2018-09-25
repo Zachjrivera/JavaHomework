c35195848be06c1631ec48516e1c38ede21f73ec
Zachjrivera <Zachjrivera@gmail.com>
2018-09-25 09:04:52 -0400

    Emplyoee homework

---
+++ src/main/java/employee/Main.java
@@ -0,0 +1,24 @@
+package employee;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        HourlyEmployee employee1 = new HourlyEmployee("Jane","Jones","HelpDesk Technician",1000,8);
+
+                System.out.println(employee1.printEmployeeProfile());
+
+
+        System.out.println("======================================================================");
+
+        Employee newEmplyoee = new Employee("Mike","Jones",100000,"Software Developer","222-222-2222");
+
+                newEmplyoee.setBonus(200);
+
+                System.out.println(newEmplyoee.printEmployeeProfile());
+        System.out.println("Employee's salary after bonus  is: $" + newEmplyoee.payCheck());
+
+
+        //=================================================================
+    }
+}
